6108a21c1ecbf2391e57f8c7581934c0bcdb792c
Denis Fulachier <44165041+dfulachier@users.noreply.github.com>
2018-10-29 16:01:15 +0100

    VS-56 - JSqlParser: Remove the "final" modifier for Column class (Rollback)

--- src/main/java/net/sf/jsqlparser/schema/Column.java
+++ src/main/java/net/sf/jsqlparser/schema/Column.java
@@ -28,7 +28,7 @@ import net.sf.jsqlparser.parser.ASTNodeAccessImpl;
 /**
  * A column. It can have the table name it belongs to.
  */
-public class Column extends ASTNodeAccessImpl implements Expression, MultiPartName {
+public final class Column extends ASTNodeAccessImpl implements Expression, MultiPartName {
 
     private Table table;
     private String columnName;
